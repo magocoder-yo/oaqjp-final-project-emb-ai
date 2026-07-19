add700ba470d0fa343d014be21a36a4806ee2721
Yo A <magoyo247@gmail.com>
2026-07-19 14:00:41 -0400

    Added Flask frontend to encapsulate function into website.

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/")
+def root_endpoint():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def emotion_detector_endpoint():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    result = emotion_detector(text_to_analyze)
+
+    return_string = f"For the given statement, the system response is "
+    return_string += f"'anger': {result['anger']}, "
+    return_string += f"'disgust': {result['disgust']}, "
+    return_string += f"'fear': {result['fear']}, "
+    return_string += f"'joy': {result['joy']} and "
+    return_string += f"'sadness': {result['sadness']}. "
+    return_string += f"The dominant emotion is {result['dominant_emotion']}."
+
+    return return_string
+
+app.run()
